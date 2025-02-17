ba2165294593fb53b7e2c05831ba08e89ae8d22f
Gabriel Fernandes <gabrielfernandes@Gabriels-MacBook-Pro.local>
2025-02-16 22:12:32 -0300

    feat: adding scripts to add and remove task

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const taskInput = document.getElementById('taskInput');
-    const addTaskBtn = document.getElementById('addTaskBtn');
-    const taskList = document.getElementById('taskList');
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
 
+document.addEventListener('DOMContentLoaded', () => {
     async function fetchTodos() {
         try {
             const res = await fetch('/api/todos');
@@ -23,6 +23,43 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('❌ Error during load todos:', err);
         }
     }
+
+    addTaskBtn.addEventListener('click', async () => {
+        const task = taskInput.value.trim();
+        if (!task) return alert('Type a task!');
+
+        try {
+            const res = await fetch('/api/todos', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ task })
+            });
+
+            if (!res.ok) throw new Error('Error adding task');
+
+            taskInput.value = '';
+            fetchTodos();
+        } catch (err) {
+            console.error('❌ Error adding task:', err);
+        }
+    });
+
+    window.deleteTodo = async (id) => {
+        try {
+            const res = await fetch(`/api/todos/${id}`, {
+                method: 'DELETE'
+            });
+            if (!res.ok) throw new Error('Error deleting task');
+
+            fetchTodos();
+        } catch (err) {
+            console.error('❌ Error deleting task:', err);
+        }
+    };
+
+    
+
+
 
     fetchTodos();
 });
